ed8b9f3a47ab9fbee1c4278f9782456992209c6a
Ashwin Ramesh <ashwin.breeks@gmail.com>
2025-07-07 13:24:08 +0000

    u dont need to recreate anythin just place this image there thats it

--- src/components/wallet-logo-icon.tsx
+++ src/components/wallet-logo-icon.tsx
@@ -20,28 +20,25 @@ export const WalletLogoIcon = ({ className }: { className?: string }) => (
         </clipPath>
       </defs>
       <g clipPath="url(#wallet-clip)">
-        <rect x="0" y="0" width="95" height="80" fill="#4285F4" />
-        <path d="M0,0 H95 V48 C 65,60 30,36 0,48 Z" fill="#EA4335" />
-        <path d="M0,0 H95 V32 C 65,44 30,20 0,32 Z" fill="#FBBC05" />
-        <rect x="0" y="0" width="95" height="16" fill="#34A853" />
+        <rect x="0" y="0" width="95" height="80" fill="#34A853" />
+        <rect x="0" y="13" width="95" height="67" fill="#FBBC05" />
+        <rect x="0" y="25" width="95" height="55" fill="#EA4335" />
+        <path d="M0,42 C 25,54 60,30 95,42 V 80 H0Z" fill="#4285F4" />
       </g>
     </g>
 
     {/* Letter 'R' */}
-    <g id="letter-r" transform="translate(100,0)">
-        {/* Blue Structure (stem and outer bowl) */}
-        <path d="M0,0 H17 V80 H0V0Z" fill="#4285F4"/>
-        <path d="M17,50 H42 C58,50 68,40 68,25 C68,10 58,0 42,0 H0V15 H42 C50,15 53,20 53,25 C53,30 50,35 42,35 H17V50Z" fill="#4285F4"/>
-        
-        {/* Red Bowl */}
-        <path d="M17,15 H42 C50,15 53,20 53,25 C53,30 50,35 42,35 H17V15Z" fill="#EA4335"/>
+    <g id="letter-r" transform="translate(100, 0)">
+      {/* Base Blue Shape of R */}
+      <path d="M0,0 H17 V80 H0Z M17,0 H45 C65,0 75,15 75,30 C75,45 65,55 45,55 H17V80 L58,80 L35,55 H17V0Z" fill="#4285F4"/>
+      
+      {/* Colored overlays on R */}
+      <path d="M17,15 H45 C58,15 64,21 64,27.5 C64,34 58,40 45,40 H17V15Z" fill="#C9493B"/>
+      <path d="M22,55 L58,80 H45 L17,60V55H22Z" fill="#34A853"/>
+      <path d="M17,55 L22,55 L17,60Z" fill="#FBBC05"/>
 
-        {/* Leg */}
-        <path d="M28,50 L58,80 H45 L21,50H28Z" fill="#34A853"/>
-        <path d="M17,50 L28,50 L23,60Z" fill="#FBBC05"/>
-
-        {/* Sparkle */}
-        <path d="M72,4 L75,11 L83,13 L75,15 L72,22 L69,15 L61,13 L69,11Z" fill="#A8C7FA"/>
+      {/* Sparkle */}
+      <path d="M72,4 L75,11 L83,13 L75,15 L72,22 L69,15 L61,13 L69,11Z" fill="#A8C7FA"/>
     </g>
   </svg>
 );
