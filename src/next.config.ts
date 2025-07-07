8fa71b82f8096a259d99eeb09c2f65901932db31
Ashwin Ramesh <ashwin.breeks@gmail.com>
2025-07-07 09:33:54 +0000

    this is the error i am getting in the console

---
+++ src/next.config.ts
@@ -0,0 +1,45 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  trailingSlash: true,
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your app.
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
